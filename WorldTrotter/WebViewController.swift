ea2cbda5202ed42bfad066bf2d8a8e950819b2b7
tstickler <tstickler17@yahoo.com>
2017-09-05 11:26:10 -0700

    Added constraints to the web view to allow for rotation

--- WorldTrotter/WebViewController.swift
+++ WorldTrotter/WebViewController.swift
@@ -21,26 +21,63 @@ class WebViewController: UIViewController, WKUIDelegate {
         
         // Sets up our web view
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.frame = CGRect(origin: CGPoint.init(x: 0, y: 60), size: view.frame.size)
 
         // Delegates how web pages are handled
         webView.uiDelegate = self
         
         // Sets the view to display the web view
         view.addSubview(webView)
-
         
+        // Used to assign constraints for the webview
+        let webTopAnchor = webView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 40)
+        let webLeadingAnchor = webView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let webTrailingAnchor = webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        let webBottomAnchor = webView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: 0)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Activates constraints
+        webTopAnchor.isActive = true
+        webLeadingAnchor.isActive = true
+        webTrailingAnchor.isActive = true
+        webBottomAnchor.isActive = true
+        
+    }
+    
+    func setUpAddrBar() {
+        // Addr bar setup includes creating the text field, sizing it and adding it to the view
         urlBar = UITextField()
-        urlBar.frame = CGRect(x: 0, y: 20, width: 300, height: 40)
+        urlBar.frame = CGRect(x: 0, y: 0, width: 0, height: 30)
+        urlBar.backgroundColor = UIColor.white
+        urlBar.borderStyle = UITextBorderStyle.roundedRect
         view.addSubview(urlBar)
+        
+        // Used to assign constraints for the text field
+        let margins = view.layoutMarginsGuide
+        let urlBarTopConstraint = urlBar.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 5)
+        let urlBarLeadingConstraint = urlBar.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 0)
+        let urlBarTrailingConstraint = urlBar.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 0)
+        urlBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Activates constraints
+        urlBarTopConstraint.isActive = true
+        urlBarLeadingConstraint.isActive = true
+        urlBarTrailingConstraint.isActive = true
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Gives a nice backgrounnd color behind the address bar instead of white
+        view.backgroundColor = UIColor.init(red: 0.92, green: 0.92, blue: 0.92, alpha: 1)
+        
+        // Sets up our view
         setUpWebView()
+        setUpAddrBar()
         
         // Sets a URL to be loaded when the webpage opens
-        let myURL = URL(string: "https://www.apple.com")
+        let urlString = "https://www.apple.com"
+        urlBar.text = urlString
+        let myURL = URL(string: urlString)
         
         /***********************************************************************************
         * To allow the web view to access non https websites, add this to apps info.plist: *
