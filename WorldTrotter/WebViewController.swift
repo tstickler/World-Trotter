036ed9b61df2b34577881396d3a45ca3ba3792b8
tyler <tstickler17@yahoo.com>
2017-09-04 21:50:49 -0700

    Fixed web view frame

--- WorldTrotter/WebViewController.swift
+++ WorldTrotter/WebViewController.swift
@@ -13,27 +13,34 @@ class WebViewController: UIViewController, WKUIDelegate {
     
     // Creates our web view
     var webView: WKWebView!
+    var urlBar: UITextField!
     
-    override func loadView() {
-        
+    func setUpWebView() {
         // Configurations to initialize web view
         let webConfiguration = WKWebViewConfiguration()
         
         // Sets up our web view
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        
+        webView.frame = CGRect(origin: CGPoint.init(x: 0, y: 60), size: view.frame.size)
+
         // Delegates how web pages are handled
         webView.uiDelegate = self
         
         // Sets the view to display the web view
-        view = webView
+        view.addSubview(webView)
+
+        
+        urlBar = UITextField()
+        urlBar.frame = CGRect(x: 0, y: 20, width: 300, height: 40)
+        view.addSubview(urlBar)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpWebView()
         
         // Sets a URL to be loaded when the webpage opens
-        let myURL = URL(string: "https://www.google.com")
+        let myURL = URL(string: "https://www.apple.com")
         
         /***********************************************************************************
         * To allow the web view to access non https websites, add this to apps info.plist: *
